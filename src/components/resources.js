274bb3b62dd293da4c0d05e6f16a5a84176130f2
Nigel Reoch <nigel.reoch@cbc.ca>
2016-03-29 13:46:19 -0400

    HOC authentication

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return(
+    <div>
+      Dallas Cowboys Offence
+      <ul>
+        <li>Tony Romo - QB</li>
+        <li>Dez Bryant - WR</li>
+        <li>Jason Witten - TE</li>
+      </ul>
+    </div>
+  );
+}
